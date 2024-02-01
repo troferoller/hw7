0ecc95395fe0abec50129b600a63f864190b5749
troferoller <d.dulger@mail.ru>
2024-02-01 11:23:47 +0300

    Reworked

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -21,12 +21,12 @@ public class TestBase extends Attach {
         Configuration.holdBrowserOpen = true;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
-        DesiredCapabilities capabilities = new DesiredCapabilities();
+        /*DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true,
                 "enableVideo", true
         ));
-        Configuration.browserCapabilities = capabilities;
+        Configuration.browserCapabilities = capabilities;*/
 
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
